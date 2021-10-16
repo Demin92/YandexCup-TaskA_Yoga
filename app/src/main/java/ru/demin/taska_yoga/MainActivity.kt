6dcda1cd35ea19242fef2cfb7f445b0c0f30e60a
Aleksei Demin <a.demin@prequelapp.com>
2021-10-16 12:28:54 +0300

    Initial commit

---
+++ app/src/main/java/ru/demin/taska_yoga/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.demin.taska_yoga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
