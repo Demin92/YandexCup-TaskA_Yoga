056dde5c126af640ce2f8ca8e6060b6719aaef75
Aleksei Demin <a.demin@prequelapp.com>
2021-10-19 16:51:38 +0300

    add report creation logic

--- app/src/main/java/ru/demin/taska_yoga/MainViewModel.kt
+++ app/src/main/java/ru/demin/taska_yoga/MainViewModel.kt
@@ -12,15 +12,17 @@ import java.io.File
 class MainViewModel(private val context: Application) : ViewModel() {
 
     private var audioRecorder: MediaRecorder? = null
+    private var averageNoise = 100
 
-    private val volumeMap = mutableMapOf<Long, Int>()
+    private val breatheTickList = mutableListOf<BreatheTick>()
+    private val report = mutableListOf<BreatheItem>()
     private val handler = Handler(Looper.getMainLooper())
     private val tickRunnable = object : Runnable {
         override fun run() {
             audioRecorder?.let { mediaRecorder ->
-                volumeMap[System.currentTimeMillis()] = mediaRecorder.maxAmplitude.also {
-                    viewState.value = viewState.value!!.copy(volume = it)
-                }
+                breatheTickList.add(BreatheTick(System.currentTimeMillis(), mediaRecorder.maxAmplitude.also { volume ->
+                    viewState.value = viewState.value!!.copy(volume = volume)
+                }))
             }
             handler.postDelayed(this, VOLUME_MEASURE_INTERVAL_IN_MILLIS)
         }
@@ -45,13 +47,55 @@ class MainViewModel(private val context: Application) : ViewModel() {
         createBreatheReport()
         sendBreatheReport()
 
-        volumeMap.onEach { Log.d("Povarity", "${it.key} ${it.value}") }
+        report.forEach {
+            Log.d("Povarity", "$it")
+        }
 
-        volumeMap.clear()
+        breatheTickList.clear()
+        report.clear()
+    }
+
+    private fun measureAverageNoise() {
+
     }
 
     private fun createBreatheReport() {
+        val inhaleThreshold = averageNoise * INHALE_KOEF
+        val exhalationThreshold = averageNoise * EXHALATION_KOEF
 
+        var lastMark: BreatheMark? = null
+        var lastTime = 0L
+
+        breatheTickList.forEachIndexed { index, tick ->
+            when {
+                index == 0 -> Unit
+                index == breatheTickList.size - 1 -> Unit
+                breatheTickList[index - 1].volume < exhalationThreshold && tick.volume >= exhalationThreshold && lastMark == BreatheMark.INHALE_END -> {
+                    lastMark = BreatheMark.EXHALATION_START
+                    report.add(BreatheItem(BreatheState.PAUSE, (tick.time - lastTime).toInt()))
+                    lastTime = tick.time
+                }
+                tick.volume >= exhalationThreshold && breatheTickList[index + 1].volume < exhalationThreshold && lastMark == BreatheMark.EXHALATION_START -> {
+                    lastMark = BreatheMark.EXHALATION_END
+                    report.add(BreatheItem(BreatheState.EXHALATION, (tick.time - lastTime).toInt()))
+                    lastTime = tick.time
+                }
+                breatheTickList[index - 1].volume < inhaleThreshold && tick.volume >= inhaleThreshold && (lastMark == null || lastMark == BreatheMark.EXHALATION_END) -> {
+                    lastMark = BreatheMark.INHALE_START
+                    if (lastTime == 0L) {
+                        lastTime = tick.time
+                    } else {
+                        report.add(BreatheItem(BreatheState.PAUSE, (tick.time - lastTime).toInt()))
+                        lastTime = tick.time
+                    }
+                }
+                tick.volume >= inhaleThreshold && breatheTickList[index + 1].volume < inhaleThreshold && lastMark == BreatheMark.INHALE_START -> {
+                    lastMark = BreatheMark.INHALE_END
+                    report.add(BreatheItem(BreatheState.INHALE, (tick.time - lastTime).toInt()))
+                    lastTime = tick.time
+                }
+            }
+        }
     }
 
     private fun sendBreatheReport() {
@@ -75,10 +119,23 @@ class MainViewModel(private val context: Application) : ViewModel() {
         audioRecorder = null
     }
 
+    private data class BreatheTick(val time: Long, val volume: Int)
+
+    private enum class BreatheMark {
+        INHALE_START, INHALE_END, EXHALATION_START, EXHALATION_END
+    }
+
+    private enum class BreatheState {
+        INHALE, PAUSE, EXHALATION
+    }
+
+    private data class BreatheItem(val state: BreatheState, val time: Int)
+
+
     companion object {
         private const val VOLUME_MEASURE_INTERVAL_IN_MILLIS = 100L
         private const val AUDIO_FILE_EXT = ".wav"
+        private const val INHALE_KOEF = 1.4f
+        private const val EXHALATION_KOEF = 4f
     }
-
-
 }
